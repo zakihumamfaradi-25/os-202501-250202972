fff09f127c54952b6b4eb5c209f2e49068788fd7
AndiPratama-250202975 <a7930642@gmail.com>
2026-01-20 12:25:17 +0700

    feat/Menambahkan Fitur CPU Scheduling dan Main Menu

---
+++ praktikum/week15-proyek-kelompok/code/cpu_scheduling.py
@@ -0,0 +1,43 @@
+import csv
+
+def simulasi_fcfs(nama_file):
+    proses = []
+
+    try:
+        with open(nama_file, newline="") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                proses.append({
+                    "nama": row["Process"],
+                    "arrival": int(row["ArrivalTime"]),
+                    "burst": int(row["BurstTime"])
+                })
+    except FileNotFoundError:
+        print("File dataset CPU tidak ditemukan!")
+        return
+
+    proses.sort(key=lambda x: x["arrival"])
+
+    waktu = 0
+    total_waiting = 0
+    total_turnaround = 0
+
+    print("\nProses | Arrival | Burst | Waiting | Turnaround")
+    print("-" * 55)
+
+    for p in proses:
+        if waktu < p["arrival"]:
+            waktu = p["arrival"]
+
+        waiting = waktu - p["arrival"]
+        turnaround = waiting + p["burst"]
+        waktu += p["burst"]
+
+        total_waiting += waiting
+        total_turnaround += turnaround
+
+        print(f"{p['nama']:9} | {p['arrival']:7} | {p['burst']:5} | {waiting:7} | {turnaround:10}")
+
+    n = len(proses)
+    print("\nRata-rata Waiting Time :", round(total_waiting / n, 2))
+    print("Rata-rata Turnaround Time :", round(total_turnaround / n, 2))
